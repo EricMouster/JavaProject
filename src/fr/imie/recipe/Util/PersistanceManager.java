62c36599fb8c5bb3a75d4cac771af6462a60bd04
noobzor <fegbarthas@gmail.com>
2018-08-07 15:19:26 +0200

    gitignore

---
+++ src/fr/imie/recipe/Util/PersistanceManager.java
@@ -0,0 +1,23 @@
+package fr.imie.recipe.Util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistanceManager {
+
+private static EntityManagerFactory emf;
+	
+	public static EntityManagerFactory getEntityManagerFactory() {
+		
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("RecipeManager_PU");
+		}
+		return emf;
+	}
+	
+	public static void closeEntityManagerFactory() {
+		if (emf != null && emf.isOpen()) {
+			emf.close();
+		}
+	}
+}
